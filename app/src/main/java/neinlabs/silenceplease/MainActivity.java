a975a8bcba4984f90dbb28f5813df0a411a6978c
saketh kaparthi <sakethkaparthi@outlook.com>
2015-03-04 21:06:01 +0530

    changing code

--- app/src/main/java/neinlabs/silenceplease/MainActivity.java
+++ app/src/main/java/neinlabs/silenceplease/MainActivity.java
@@ -4,26 +4,17 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
 
-public class MainActivity extends ActionBarActivity{
-    private GoogleMap googleMap;
+public class MainActivity extends ActionBarActivity implements OnMapReadyCallback{
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        try {
-            // Loading map
-            initilizeMap();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
     }
      @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,26 +37,10 @@ public class MainActivity extends ActionBarActivity{
 
         return super.onOptionsItemSelected(item);
     }
-    private void initilizeMap() {
-        if (googleMap == null) {
-            googleMap = ((MapFragment) getFragmentManager().findFragmentById(
-                    R.id.map)).getMap();
 
-            // check if map is created successfully or not
-            if (googleMap == null) {
-                Toast.makeText(getApplicationContext(),
-                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
-                        .show();
-            }
-            googleMap.setMyLocationEnabled(true);
-        }
-    }
+
     @Override
-    protected void onResume() {
-        super.onResume();
-        initilizeMap();
+    public void onMapReady(GoogleMap googleMap) {
+
     }
-
-
-
 }
