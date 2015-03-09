09269c09a7291f5fa7290aa44f76290d3daac081
saketh kaparthi <sakethkaparthi@outlook.com>
2015-03-09 16:23:15 +0530

    manually copied test2 things to master

--- app/src/main/java/neinlabs/silenceplease/MainActivity.java
+++ app/src/main/java/neinlabs/silenceplease/MainActivity.java
@@ -5,7 +5,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.media.AudioManager;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,6 +24,17 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import neinlabs.silenceplease.buttons.FloatingActionButton;
 
 
@@ -27,6 +42,8 @@ public class MainActivity extends Activity implements OnMapReadyCallback,GoogleM
 
     GoogleMap myMap;
     SQLiteDatabase mydb;
+    Double distance;
+    private AudioManager myAudioManager;
     private static String DBNAME = "PERSONS.db";    // THIS IS THE SQLITE DATABASE FILE NAME.
     private static String TABLE = "MY_TABLE";       // THIS IS THE TABLE NAME
     @Override
@@ -51,7 +68,14 @@ public class MainActivity extends Activity implements OnMapReadyCallback,GoogleM
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
+    public void CheckIfInRange(Double d){
+         myAudioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+         if(d<100.0){
+             myAudioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+             }else{
+             myAudioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+             }
+         }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -71,12 +95,9 @@ public class MainActivity extends Activity implements OnMapReadyCallback,GoogleM
     @Override
     public void onMapReady(GoogleMap googleMap) {
         googleMap.setMyLocationEnabled(true);
-
-
-            MapFragment mapFragment = (MapFragment) getFragmentManager()
-                    .findFragmentById(R.id.map);
-            myMap = mapFragment.getMap();
-            myMap.setOnMapClickListener(this);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        myMap = mapFragment.getMap();
+        myMap.setOnMapClickListener(this);
         myMap.clear();
         myMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
         mydb = openOrCreateDatabase(DBNAME, Context.MODE_PRIVATE, null);
@@ -104,6 +125,7 @@ public class MainActivity extends Activity implements OnMapReadyCallback,GoogleM
         view.startAnimation(animation);
     }
 
+    String format = null;
 
     @Override
     public void onMapClick(final LatLng latLng) {
@@ -135,5 +157,120 @@ public class MainActivity extends Activity implements OnMapReadyCallback,GoogleM
                 startActivity(new Intent(MainActivity.this, DatabaseHelper.class));
             }
         });
+        FetchWeatherTask weatherTask = new FetchWeatherTask();
+         android.location.Location lt = myMap.getMyLocation();
+         String latlng = String.valueOf(lt.getLatitude()) + "," + String.valueOf(lt.getLongitude());
+         format = String.valueOf(latLng.latitude) + "," + String.valueOf(latLng.longitude);
+         weatherTask.execute(latlng);
+         Log.d("ok","from mapclick " + String.valueOf(distance));
        }
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, String> {
+
+                 private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+
+                 private String getWeatherDataFromJson(String forecastJsonStr)
+         throws JSONException {
+            JSONObject data = new JSONObject(forecastJsonStr);
+            JSONArray rows = data.getJSONArray("rows");
+             JSONObject lol = rows.getJSONObject(0);
+             JSONArray elements = lol.getJSONArray("elements");
+             JSONObject k = elements.getJSONObject(0);
+             JSONObject distance = k.getJSONObject("distance");
+             double p = distance.getDouble("value");
+             Log.d(LOG_TAG, "value : " + String.valueOf(p));
+             return String.valueOf(p);
+             }
+
+                @Override
+         protected String doInBackground(String... params) {
+
+                    // If there's no zip code, there's nothing to look up. Verify size of params.
+                             if (params.length == 0) {
+                 return null;
+                 }
+
+                    // These two need to be declared outside the try/catch
+                             // so that they can be closed in the finally block.
+                                    HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+                   // Will contain the raw JSON response as a string
+                             String forecastJsonStr = null;
+             try {
+                 // Construct the URL for the OpenWeatherMap query
+                         // Possible parameters are avaiable at OWM's forecast API page, at
+                                 // http://openweathermap.org/API#forecast
+                                         final String FORECAST_BASE_URL =
+                         "https://maps.googleapis.com/maps/api/distancematrix/json?";
+                 final String QUERY_PARAM = "origins";
+                 final String FORMAT_PARAM = "destinations";
+
+                        Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                         .appendQueryParameter(QUERY_PARAM, params[0])
+                        .appendQueryParameter(FORMAT_PARAM,     format)
+                        .build();
+
+                      URL url = new URL(builtUri.toString());
+
+                         Log.v(LOG_TAG, "Built URI " + builtUri.toString());
+                                     // Create the request to OpenWeatherMap, and open the connection
+                                 urlConnection = (HttpURLConnection) url.openConnection();
+                 urlConnection.setRequestMethod("GET");
+                 urlConnection.connect();
+
+                         // Read the input stream into a String
+                                InputStream inputStream = urlConnection.getInputStream();
+                 StringBuffer buffer = new StringBuffer();
+                 if (inputStream == null) {
+                     // Nothing to do.
+                             return null;
+                     }
+                 reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                         String line;
+                 while ((line = reader.readLine()) != null) {
+                     // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                             // But it does make debugging a *lot* easier if you print out the completed
+                                     // buffer for debugging.
+                                             buffer.append(line + "\n");
+                     }
+
+                         if (buffer.length() == 0) {
+                     // Stream was empty. No point in parsing.
+                             return null;
+                     }
+                 forecastJsonStr = buffer.toString();
+                } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                 // If the code didn't successfully get the weather data, there's no point in attemping
+                         // to parse it.
+                                 return null;
+                 } finally {
+                 if (urlConnection != null) {
+                     urlConnection.disconnect();
+                     }
+                 if (reader != null) {
+                    try {
+                         reader.close();
+                         } catch (final IOException e) {
+                         Log.e(LOG_TAG, "Error closing stream", e);
+                         }
+                     }
+                 }
+             try {
+                 return getWeatherDataFromJson(forecastJsonStr);
+                 } catch (JSONException e) {
+                 e.printStackTrace();
+                 }
+
+                     return null;
+             }
+
+                 @Override
+         protected void onPostExecute(String result) {
+             CheckIfInRange(Double.parseDouble(result));
+             }
+
+           }
 }
